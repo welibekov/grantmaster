556399ef7e36ae98b26daf4862b1255ce0faf6b6
Eldar Valibayov <ext.Eldar.Valibayov@exasol.com>
2025-02-21 19:33:11 +0400

    Add `role` subcommand; move `policy` under `apply`

--- code/cmd/apply.go
+++ code/cmd/apply.go
@@ -1,65 +1,22 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
-	"strings"
 
 	"github.com/spf13/cobra"
-	"github.com/welibekov/grantmaster/modules/database"
-	"github.com/welibekov/grantmaster/modules/utils"
 )
 
 var policyFile string
 
 func init() {
-	rootCmd.AddCommand(gmCmd)
-	gmCmd.Flags().StringVar(&policyFile, "policy", "", "Path to policy YAML file (mandatory)")
-	gmCmd.MarkFlagRequired("policy")
+	rootCmd.AddCommand(gmApplyCmd)
 }
 
-var gmCmd = &cobra.Command{
+var gmApplyCmd = &cobra.Command{
 	Use:   "apply",
-	Short: "Apply policies from the specified YAML file",
+	Short: "Apply policies,roles from the specified YAML file or directory",
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := run(); err != nil {
-			fmt.Println("Error:", err)
-		}
+		cmd.Help()
+		os.Exit(1)
 	},
-}
-
-func run() error {
-	// Load configuration from environment variables
-	config := loadConfig()
-
-	// Read policies from file or directory.
-	policies, err := utils.ReadPolicies(policyFile)
-	if err != nil {
-		return fmt.Errorf("couldn't read policies: %v", err)
-	}
-
-	// Detect duplicated policies
-	if err := utils.DetectDuplicated(policies); err != nil {
-		return fmt.Errorf("duplicated policies found: %v", err)
-	}
-
-	// Create an instance of database
-	databaseInstance, err := database.New(config)
-	if err != nil {
-		return fmt.Errorf("failed to create database instance: %w", err)
-	}
-
-	// Apply policies
-	return databaseInstance.Apply(policies)
-}
-
-func loadConfig() map[string]string {
-	config := make(map[string]string)
-	for _, env := range os.Environ() {
-		kv := strings.SplitN(env, "=", 2)
-		if len(kv) == 2 && strings.HasPrefix(kv[0], "GM_") {
-			config[kv[0]] = kv[1]
-		}
-	}
-	return config
 }
