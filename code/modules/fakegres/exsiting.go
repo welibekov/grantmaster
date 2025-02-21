e815c9f9de6a3ae85f117993d73dffe0b4624ea4
Eldar Valibayov <ext.Eldar.Valibayov@exasol.com>
2025-02-21 18:15:58 +0400

    Modularize yaml files

--- code/modules/fakegres/exsiting.go
+++ code/modules/fakegres/exsiting.go
@@ -3,6 +3,7 @@ package fakegres
 import (
 	"fmt"
 
+	"github.com/welibekov/grantmaster/modules/types"
 	"github.com/welibekov/grantmaster/modules/utils"
 )
 
@@ -10,7 +11,18 @@ import (
 func (f *Fakegres) readExistingPolicies() (map[string][]string, error) {
 	policiesMap := make(map[string][]string)
 
-	policies, err := utils.ReadPoliciesFromDirectory(f.rootDir)
+	policies, err := utils.ReadPoliciesFromDirectory(f.rootDir,
+		func(path string) ([]types.Policy, error) {
+			policies := []types.Policy{}
+
+			policy, err := utils.ReadPolicy[types.Policy](path)
+			if err != nil {
+				return policies, err
+			}
+
+			return append(policies, policy), nil
+		})
+
 	if err != nil {
 		return nil, fmt.Errorf("error walking the path %q: %w", f.rootDir, err)
 	}
