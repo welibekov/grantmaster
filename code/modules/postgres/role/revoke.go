31708d2efe85530b61b4f0ffc414f4363f8b31dc
Eldar Valibayov <ext.Eldar.Valibayov@exasol.com>
2025-03-02 23:03:48 +0400

    Remove unused code

--- code/modules/postgres/role/revoke.go
+++ code/modules/postgres/role/revoke.go
@@ -26,34 +26,3 @@ func (p *PGRole) Revoke(ctx context.Context, roles []types.Role) error {
 
 	return nil
 }
-
-//func (p *PGRole) RevokeOldVersion(ctx context.Context, roles []types.Role) error {
-//	for _, role := range roles {
-//		query := p.revokeQuery(role)
-//
-//		logrus.Debugln(query) // Log the generated query for debugging purposes
-//
-//		// Execute the revoke query on the PostgreSQL database
-//		_, err := p.pool.Exec(ctx, query)
-//		if err != nil {
-//			// Wrap the error with more context to help identify the issue if it occurs
-//			return fmt.Errorf("couldn't revoke grants from role'%s': %w", role.Name, err)
-//		}
-//	}
-//
-//	// Return nil if all policies were processed without errors
-//	return nil
-//}
-//
-//func (p *PGRole) revokeQuery(role types.Role) string {
-//	var query string
-//	for _, schema := range role.Schemas {
-//		query += fmt.Sprintf(`REVOKE ALL PRIVILEGES ON SCHEMA %s FROM %s;`, schema.Schema, role.Name)
-//		query += fmt.Sprintf(`REVOKE GRANT OPTION FOR ALL PRIVILEGES ON SCHEMA %s FROM %s;`, schema.Schema, role.Name)
-//	}
-//
-//	//FIXME: Do we need to drop role at the end?
-//	//query += fmt.Sprintf(`DROP ROLE %s`, role.Name)
-//
-//	return query
-//}
