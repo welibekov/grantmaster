3107acc76f22770b8f3c184df4743d1c289d7c03
Eldar Valibayov <ext.Eldar.Valibayov@exasol.com>
2025-03-20 14:18:44 +0400

    Add roles tests

--- tests/postgres/role/apply.bash
+++ tests/postgres/role/apply.bash
@@ -1,71 +1,39 @@
 #!/bin/bash
 
-export POSTGRES_DOCKER_CONTAINER=${POSTGRES_DOCKER_CONTAINER:-postgres}
-export POSTGRES_USER=${POSTGRES_USER:-dwh}
-export POSTGRES_DATABASE=${POSTGRES_DATABASE:-dwh}
-export GM_DATABASE_TYPE=postgres
-export GM_POSTGRES_CONN_STRING='postgres://dwh:dwh@localhost:5432/dwh'
-
 export TEST_DIR=$(mktemp -d)
 
-_exec() {
-  docker exec -ti "$POSTGRES_DOCKER_CONTAINER" "$@"
-}
-
-_psql_read() {
-  _exec cat /root/output
-}
-
-_psql() {
-  _exec psql --output=/root/output -U "$POSTGRES_USER" -d "$POSTGRES_DATABASE" "$@"
-}
-
-_role() {
-  $GM_BIN role "$@"
-}
-
-_cmp() {
-  cmp -s "$@"
-}
-
 main() {
+  source tests/postgres/role/_tests
+
+  cat <<EOF
+/////////////////////////////////
+//                             //
+// Starting role tests         //
+// Results are under:          //
+// $TEST_DIR/roles   //
+//                             //
+/////////////////////////////////
+EOF
+  tests=()
+  
+  while IFS= read -r test; do
+    tests+=("$test")
+  done <<<$(declare -F | grep -Eo "_test_number_[0-9]{1,}")
+
+  for test_name in "${tests[@]}"; do
+    local num; num=${test_name##*_}
+
+    "$test_name" "$num" 
+  done
+
   cat <<'EOF'
-/////////////////////////
-//                     //
-// Starting role tests //
+/////////////////////////////////
+//                             //
+// Stopping role tests         //
+//                             //
+/////////////////////////////////
 EOF
-
-  _test_number_one
 }
-
-_test_number_one() {
-  echo -n "> Test #1 ... "
-
-  cat <<'EOF' >"$TEST_DIR/role.yaml"
-- name: song_read
-  schemas:
-    - schema: song
-      grants:
-        - usage
-EOF
-
-  _role apply "$TEST_DIR/role.yaml" || exit 1
-  _role get > "$TEST_DIR/get_roles.yaml" || exit 1
-
- local exitcode; exitcode=0
-
- if ! _cmp "$TEST_DIR/role.yaml" "$TEST_DIR/get_roles.yaml"; then
-   diff "$TEST_DIR/role.yaml" "$TEST_DIR/get_roles.yaml"
-   exitcode=1
- fi
-
- if [[ $exitcode -eq 0 ]]; then
-   echo done
- else
-   echo failed
- fi
-}
-
 
 main "$@"
 
