78da297a08857fd5a026b0a3ef7887e5339cdb45
Eldar Valibayov <ext.Eldar.Valibayov@exasol.com>
2025-03-21 13:37:24 +0400

    Refactoring in role and policy modules

--- code/modules/policy/policy.go
+++ code/modules/policy/policy.go
@@ -4,24 +4,25 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/welibekov/grantmaster/modules/config"
 	"github.com/welibekov/grantmaster/modules/databaser"
-	//fakegres "github.com/welibekov/grantmaster/modules/fakegres/role"
 	//greenplum "github.com/welibekov/grantmaster/modules/greenplum/role"
+	fgPol "github.com/welibekov/grantmaster/modules/fakegres/policy"
 	pgPol "github.com/welibekov/grantmaster/modules/postgres/policy"
 	"github.com/welibekov/grantmaster/modules/types"
 )
 
-func New(ctx context.Context, config map[string]string) (databaser.Policier, error) {
-	switch types.DatabaseType(config["GM_DATABASE_TYPE"]) {
+func New(ctx context.Context, cfg map[string]string) (databaser.Policier, error) {
+	switch types.DatabaseType(cfg[config.DatabaseType]) {
 	case types.Postgres:
 		// Initialize Postgres database
-		return pgPol.New(ctx, config)
-	//case types.Fakegres:
-	// Initialize Fakegres database
-	//	return fakegres.New(config)
+		return pgPol.New(ctx, cfg)
+	case types.Fakegres:
+		// Initialize Fakegres database
+		return fgPol.New(cfg)
 	// Initialize Greenplum database
 	//case types.Greenplum:
-	//	return greenplum.New(config)
+	//	return greenplum.New(cfg)
 	default:
 		// Return an error if the database type is not recognized
 		return nil, fmt.Errorf("database type could not be found: GM_DATABASE_TYPE")
