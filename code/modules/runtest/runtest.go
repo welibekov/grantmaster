8812cbeae62b755881d5f515f782bc25c8097c1a
Eldar Valibayov <ext.Eldar.Valibayov@exasol.com>
2025-03-22 11:06:18 +0400

    Add runtest implementation for fakegres

--- code/modules/runtest/runtest.go
+++ code/modules/runtest/runtest.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/welibekov/grantmaster/modules/databaser"
+	fgRuntest "github.com/welibekov/grantmaster/modules/fakegres/runtest"
 	pgRuntest "github.com/welibekov/grantmaster/modules/postgres/runtest"
 	"github.com/welibekov/grantmaster/modules/types"
 )
@@ -12,6 +13,8 @@ func New(dbType types.DatabaseType, tests []string) (databaser.RunTesterer, erro
 	switch dbType {
 	case types.Postgres:
 		return pgRuntest.New(tests)
+	case types.Fakegres:
+		return fgRuntest.New(tests)
 	default:
 		return nil, fmt.Errorf("no database type %s found", dbType)
 	}
