86a40e70535c19718d73b1df9a0482de35ba9706
Eldar Valibayov <ext.Eldar.Valibayov@exasol.com>
2025-03-24 00:40:45 +0400

    Add more role tests

--- tests/postgres/role/apply.bash
+++ tests/postgres/role/apply.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 main() {
-  source tests/postgres/_functions
+  for func in tests/postgres/_functions tests/postgres/role/_functions; do
+    source "$func"
+  done
   
   _load_and_run_tests role
    
