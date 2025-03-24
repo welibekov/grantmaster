0b0ba3d2f123a9160e76e1771e10f26d4f3fea83
Eldar Valibayov <ext.Eldar.Valibayov@exasol.com>
2025-03-25 02:14:28 +0400

    Add missing comments: runtest module

--- code/modules/runtest/runtest.go
+++ code/modules/runtest/runtest.go
@@ -9,6 +9,13 @@ import (
 	"github.com/welibekov/grantmaster/modules/types"
 )
 
+// New creates a new instance of RunTesterer based on the provided database type.
+// It initializes and returns the appropriate testing implementation for the specified database.
+// If the dbType is not recognized, it returns an error indicating the database type is not supported.
+//
+// Currently, this function only supports two database types: Postgres and Fakegres. 
+// If additional database types are required in the future, they should be added to this 
+// switch statement along with their corresponding initialization logic.
 func New(dbType types.DatabaseType, tests []string) (databaser.RunTesterer, error) {
 	switch dbType {
 	case types.Postgres:
