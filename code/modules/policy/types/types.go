26a717dd52ae62d4f5347f84d936b7eef7ea38ff
Eldar Valibayov <ext.Eldar.Valibayov@exasol.com>
2025-03-25 02:33:34 +0400

    Add missing comments: policy module

--- code/modules/policy/types/types.go
+++ code/modules/policy/types/types.go
@@ -4,5 +4,5 @@ package types
 // specifying which actions they can perform.
 type Policy struct {
 	Username string   `yaml:"username"` // The username associated with the policy.
-	Roles    []string `yaml:"roles"`    // List of roles allowed to perform the action.
+	Roles    []string `yaml:"roles"`    // List of roles assigned to the user.
 }
--- code/modules/policy/utils/diff.go
+++ code/modules/policy/utils/diff.go
@@ -10,17 +10,20 @@ func Diff(refPolicies, candPolicies []types.Policy) []types.Policy {
 	// Build a lookup map for reference policies by Username.
 	refMap := make(map[string]types.Policy)
 	for _, p := range refPolicies {
-		refMap[p.Username] = p
+		refMap[p.Username] = p // Store each reference policy by its Username.
 	}
-	var diff []types.Policy
+
+	var diff []types.Policy // Slice to hold the policies that differ or are missing.
+
 	for _, candPolicy := range candPolicies {
 		if refPolicy, ok := refMap[candPolicy.Username]; !ok {
-			// This policy is new in the candidate.
+			// This policy is new in the candidate because it does not exist in reference.
 			diff = append(diff, candPolicy)
 		} else {
 			// Compare roles: get roles that are in candidate but not in reference.
 			rolesDiff := diffRoles(refPolicy.Roles, candPolicy.Roles)
 			if len(rolesDiff) > 0 {
+				// Create a new policy with the differing roles and add it to the diff slice.
 				diff = append(diff, types.Policy{
 					Username: candPolicy.Username,
 					Roles:    rolesDiff,
@@ -28,20 +31,23 @@ func Diff(refPolicies, candPolicies []types.Policy) []types.Policy {
 			}
 		}
 	}
-	return diff
+	return diff // Return the slice of policies that are different or missing.
 }
 
 // diffRoles returns the roles present in candRoles that do not appear in refRoles.
 func diffRoles(refRoles, candRoles []string) []string {
-	refSet := make(map[string]bool)
+	refSet := make(map[string]bool) // Create a set for quick lookup of reference roles.
 	for _, role := range refRoles {
-		refSet[role] = true
+		refSet[role] = true // Add each role from refRoles to the set.
 	}
-	var diff []string
+
+	var diff []string // Slice to hold roles that are different.
+
 	for _, role := range candRoles {
 		if !refSet[role] {
+			// If the role is not found in the reference set, add it to the diff slice.
 			diff = append(diff, role)
 		}
 	}
-	return diff
+	return diff // Return the list of roles that differ.
 }
