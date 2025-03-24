b1e91e2532e816dfe4fb148987b1d51fa5a19ea5
Eldar Valibayov <ext.Eldar.Valibayov@exasol.com>
2025-03-25 02:24:56 +0400

    Add missing comments: types module

--- code/modules/types/types.go
+++ code/modules/types/types.go
@@ -4,13 +4,20 @@ package types
 type DatabaseType string
 
 var (
-	Postgres  DatabaseType = "postgres"  // Postgres is a constant for the PostgreSQL database type.
-	Greenplum DatabaseType = "greenplum" // Greenplum is a constant for the Greenplum database type.
-	Fakegres  DatabaseType = "fakegres"  // Fakegres is a constant for a mocked or fake database type.
+	// Postgres is a constant for the PostgreSQL database type.
+	Postgres DatabaseType = "postgres"  
+	
+	// Greenplum is a constant for the Greenplum database type.
+	Greenplum DatabaseType = "greenplum" 
+	
+	// Fakegres is a constant for a mocked or fake database type.
+	Fakegres DatabaseType = "fakegres"  
 )
 
+// Databases is a slice containing all supported DatabaseType constants.
 var Databases = []DatabaseType{Postgres, Greenplum, Fakegres}
 
+// ToString returns the string representation of the DatabaseType.
 func (d DatabaseType) ToString() string {
 	return string(d)
 }
